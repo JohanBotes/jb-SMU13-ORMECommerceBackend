300d9f566a56a21be2180f7f6a6d06148d6d0873
Johan Botes <johan.botes@udexx.com>
2022-10-06 17:16:27 -0500

    2nd Commit - Error connect to database

--- models/Product.js
+++ models/Product.js
@@ -1,5 +1,8 @@
 // import important parts of sequelize library
+const { DECIMAL } = require('sequelize');
+const { NUMERIC } = require('sequelize');
 const { Model, DataTypes } = require('sequelize');
+
 // import our database connection from config.js
 const sequelize = require('../config/connection');
 
@@ -9,7 +12,46 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+  }
+  },
+  {
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+  }
+  },
+  {
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        is: DECIMAL
+      },
+  }
+  },
+  {
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      default: 10,
+      validate: {
+        is: NUMERIC
+      }
+  }
+  },
+  {
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "category",
+        key: "id"
+      },  
+  }
   },
   {
     sequelize,
